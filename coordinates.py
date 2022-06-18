4b38bd994e6792674d3404da915368db5f208a6d
artem.zavyalov <artem.zavyalov@gmail.com>
2022-06-18 14:04:41 -0700

    Black formatter applied

--- coordinates.py
+++ coordinates.py
@@ -1,36 +1,44 @@
 #!/usr/bin/env python3
 
 import argparse
-from geopy.geocoders import Nominatim #pip install Nominatim geopy
+from geopy.geocoders import Nominatim  # pip install Nominatim geopy
 from geopy.exc import GeocoderTimedOut
+
 
 def give_me_coordinates(address):
     try:
-        geolocator = Nominatim(user_agent='coordinates.py')
+        geolocator = Nominatim(user_agent="coordinates.py")
         location = geolocator.geocode(address, timeout=10)
         return (location.latitude, location.longitude)
     except AttributeError:
-        raise ValueError('Problem with address. Cannot Geocode.')
+        raise ValueError("Problem with address. Cannot Geocode.")
     except GeocoderTimedOut:
-        raise TimeoutError('Timeout for location request.')
+        raise TimeoutError("Timeout for location request.")
+
 
 def give_me_details(address):
     try:
-        geolocator = Nominatim(user_agent='coordinates.py')
+        geolocator = Nominatim(user_agent="coordinates.py")
         location = geolocator.geocode(address, timeout=10)
-        return (location.address)
+        return location.address
     except AttributeError:
-        raise ValueError('Problem with address. Cannot Geocode.')
+        raise ValueError("Problem with address. Cannot Geocode.")
     except GeocoderTimedOut:
-        raise TimeoutError('Timeout for location request.')
+        raise TimeoutError("Timeout for location request.")
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Geocoding addresses to coordinates:')
-    parser.add_argument('-a','--address', help='Provide your location, like full address, or city with region', required=True)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Geocoding addresses to coordinates:")
+    parser.add_argument(
+        "-a",
+        "--address",
+        help="Provide your location, like full address, or city with region",
+        required=True,
+    )
     args = parser.parse_args()
 
     try:
-        print("Details:",give_me_details(args.address))
-        print("Coordinates:",give_me_coordinates(args.address))
+        print("Details:", give_me_details(args.address))
+        print("Coordinates:", give_me_coordinates(args.address))
     except Exception as e:
         print(e)
--- when-should-i-wash-the-car.py
+++ when-should-i-wash-the-car.py
@@ -12,7 +12,7 @@ from pyweatherbitdata.api import WeatherBitApiClient
 from pyweatherbitdata.data import (
     ObservationDescription,
     BaseDataDescription,
-    ForecastDescription
+    ForecastDescription,
 )
 from pyweatherbitdata.exceptions import (
     InvalidApiKey,
@@ -22,69 +22,89 @@ from pyweatherbitdata.exceptions import (
 
 _LOGGER = logging.getLogger(__name__)
 
+
 def average(lst):
     return sum(lst) / len(lst)
 
-def list_less(lst,less):
+
+def list_less(lst, less):
     for x in lst:
-        if(x>less):
+        if x > less:
             return 0
     return 1
 
-async def wash_or_not_to_wash(pop_list,threshold,duration):
+
+async def wash_or_not_to_wash(pop_list, threshold, duration):
     result = []
     p_list = []
     p_result = []
     for x in range(len(pop_list)):
         p_list.append(pop_list[x])
-        if(len(p_list)>=duration):
-            if(len(p_result)==5):
-                if(p_result[3]>average(p_list)):
-                    p_result = [duration, threshold, x-1, average(p_list), max(p_list)]
+        if len(p_list) >= duration:
+            if len(p_result) == 5:
+                if p_result[3] > average(p_list):
+                    p_result = [
+                        duration,
+                        threshold,
+                        x - 1,
+                        average(p_list),
+                        max(p_list),
+                    ]
             else:
-                p_result = [duration, threshold, x-1, average(p_list), max(p_list)]
-            if(list_less(p_list,threshold)):
-                result.append([duration, x-1, p_list.copy(), average(p_list), max(p_list)])
+                p_result = [duration, threshold, x - 1, average(p_list), max(p_list)]
+            if list_less(p_list, threshold):
+                result.append(
+                    [duration, x - 1, p_list.copy(), average(p_list), max(p_list)]
+                )
             p_list.pop(0)
-    result.insert(0,p_result)
+    result.insert(0, p_result)
     return result
 
+
 async def prettify_wash(wash_list):
-    if len(wash_list)>1:
-        str="Based on {} day(s) period, best time to wash your car is in {} day(s) " \
+    if len(wash_list) > 1:
+        str = (
+            "Based on {} day(s) period, best time to wash your car is in {} day(s) "
             "with average of {:.2f}% chance (maximum {}% pop) of rain during this period."
-        print(str.format(
-                wash_list[1][0],
-                wash_list[1][1],
-                wash_list[1][3],
-                wash_list[1][4]
-                ))
-    elif len(wash_list)==1:
-        str="Unfortunately based on {} day(s) period, no good time to wash your car (with less then {:.2f}% pop), " \
+        )
+        print(
+            str.format(
+                wash_list[1][0], wash_list[1][1], wash_list[1][3], wash_list[1][4]
+            )
+        )
+    elif len(wash_list) == 1:
+        str = (
+            "Unfortunately based on {} day(s) period, no good time to wash your car (with less then {:.2f}% pop), "
             "best time to wash is in {} day(s) when average chance of rain will be {:.2f}% but with maximum change of {}."
-        print(str.format(
+        )
+        print(
+            str.format(
                 wash_list[0][0],
                 wash_list[0][1],
                 wash_list[0][2],
                 wash_list[0][3],
-                wash_list[0][4]
-                ))
+                wash_list[0][4],
+            )
+        )
     else:
         print("Something wrong")
 
-async def check_weather(address,key,lang):
+
+async def check_weather(address, key, lang):
     from coordinates import give_me_coordinates
+
     start = time.time()
-    (latitude,longitude)=give_me_coordinates(address)
+    (latitude, longitude) = give_me_coordinates(address)
 
     weatherbit = WeatherBitApiClient(
         key,
-        latitude, longitude,
+        latitude,
+        longitude,
         language=lang,
     )
 
     try:
-       await weatherbit.initialize()
+        await weatherbit.initialize()
 
     except InvalidApiKey as err:
         _LOGGER.debug(err)
@@ -95,10 +115,10 @@ async def check_weather(address,key,lang):
 
     data: ForecastDescription = await weatherbit.update_forecast()
     await weatherbit.req.close()
-    
+
     end = time.time()
     _LOGGER.debug("Execution time: %s seconds", end - start)
-    
+
     if data is not None:
         pop = []
         for x in range(len(data.forecast)):
@@ -106,10 +126,13 @@ async def check_weather(address,key,lang):
 
         _LOGGER.debug("Weather data is %s", data)
         _LOGGER.debug("POP schedule %s", pop)
-     
+
         return pop
     else:
-        raise ValueError('Problem with weather. Cannot get information for the location.')
+        raise ValueError(
+            "Problem with weather. Cannot get information for the location."
+        )
+
 
 async def main():
     weather_pops = await check_weather(args.address, args.key, "en")
@@ -117,18 +140,40 @@ async def main():
     pretty = await prettify_wash(wash)
     return pretty
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Should you wash a car today?')
-    parser.add_argument('-a','--address', help='Provide your location, like full address, or city with region (Coordinates are allowed too)', required=True)
-    parser.add_argument('-d','--days', help='How many days you want to keep your car clean? (Default 3 days)', type=int, default='3')
-    parser.add_argument('-p','--percentage', help="What average probability of precipitation you want to use? (Default 50%%)", type=int, default='50')
-    parser.add_argument('--key',help="WeatherBitApiClient key if you have it", default='12c4f93fc60a4161b0685bad13519735')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Should you wash a car today?")
+    parser.add_argument(
+        "-a",
+        "--address",
+        help="Provide your location, like full address, or city with region (Coordinates are allowed too)",
+        required=True,
+    )
+    parser.add_argument(
+        "-d",
+        "--days",
+        help="How many days you want to keep your car clean? (Default 3 days)",
+        type=int,
+        default="3",
+    )
+    parser.add_argument(
+        "-p",
+        "--percentage",
+        help="What average probability of precipitation you want to use? (Default 50%%)",
+        type=int,
+        default="50",
+    )
+    parser.add_argument(
+        "--key",
+        help="WeatherBitApiClient key if you have it",
+        default="12c4f93fc60a4161b0685bad13519735",
+    )
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.INFO)
 
-    #TODO: add arguments to the function
-    #TODO: rename function to something unique
-#    asyncio.run(check_car_wash(args.days, args.percentage, args.address, args.key, "en"))
+    # TODO: add arguments to the function
+    # TODO: rename function to something unique
+    #    asyncio.run(check_car_wash(args.days, args.percentage, args.address, args.key, "en"))
 
     asyncio.run(main())
